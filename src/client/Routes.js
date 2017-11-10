fcd4ac0d79594166bc8e7f84b8f24d829c1babc7
Spencer Snyder <sasnyde2@gmail.com>
2017-11-10 02:06:07 -0500

    SSR with RR4

---
+++ src/client/Routes.js
@@ -0,0 +1,11 @@
+import React from 'React';
+import { Route } from 'react-router-dom';
+import Home from './components/Home';
+
+export default () => {
+  return (
+    <div>
+      <Route exact path="/" component={Home} />
+    </div>
+  );
+};
--- src/client/client.js
+++ src/client/client.js
@@ -1,6 +1,12 @@
 // Startup point for client side app!
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './components/Home';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './Routes';
 
-ReactDOM.hydrate(<Home />, document.getElementById('root'));
+ReactDOM.hydrate(
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
